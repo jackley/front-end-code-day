5e2338b604d15bc480c45c91ce5fa09e41f66017
James Ackley <james.ackley@gmail.com>
2017-10-08 07:54:38 -0400

    Corrections to pass linting

--- dribbble-feed/src/assets/scripts/main.js
+++ dribbble-feed/src/assets/scripts/main.js
@@ -28,24 +28,24 @@ const DOM = {
 };
 
 
-	/**
-	 * Return a templated feed.
-	 * The `feed` module is function that returns a template literal.
-	 * @param  {array}  data Context for template
-	 * @return {string} Templated HTML
-	 */
-	function buildFeed(data) {
-		// template using data; return a string of HTML
-		return feedTemplate(data);
-	}
+/**
+ * Return a templated feed.
+ * The `feed` module is function that returns a template literal.
+ * @param  {array}  data Context for template
+ * @return {string} Templated HTML
+ */
+function buildFeed(data) {
+	// template using data; return a string of HTML
+	return feedTemplate(data);
+}
 
 
-	// TODO add event handler to button
-	// TODO make an API call to Dribble
-	// TODO pass data to template
+// TODO add event handler to button
+// TODO make an API call to Dribble
+// TODO pass data to template
 
 
-	// @example pass data from API response to Handlebars template
-	// TODO remove this example
-	console.log(buildFeed([]));
+// @example pass data from API response to Handlebars template
+// TODO remove this example
+console.log(buildFeed([]));
 
